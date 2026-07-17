0f206e4cc559145dd53cca1d70a57f8f9fea0b89
Sami Walbury <sami@steadfastcollective.com>
2026-07-17 19:22:29 +0100

    Improve install-on-badge by not copying __pycache__

--- thunderdome-tildagon-app/install-on-badge.py
+++ thunderdome-tildagon-app/install-on-badge.py
@@ -1,4 +1,7 @@
 #!/bin/sh
 mpremote mkdir apps/thunderdome
-mpremote cp -r ./* :apps/thunderdome/
+for f in ./*; do
+    [ "$f" = "./__pycache__" ] && continue
+    mpremote cp "$f" :apps/thunderdome/
+done
 mpremote reset
